d833cf63fe064496c8dc18f491707b5437193704
Agustín Ramos <zentimental.software@gmail.com>
2021-12-03 08:33:55 -0600

    Refactored tests for day 3

--- test/03/binary_diagnostic_test.exs
+++ test/03/binary_diagnostic_test.exs
@@ -1,8 +1,8 @@
 defmodule BinaryDiagnosticTest do
   use ExUnit.Case
 
-  test "1st part. Sampel input" do
-    input =
+  setup do
+    sample =
       [
         "00100",
         "11110",
@@ -17,41 +17,25 @@ defmodule BinaryDiagnosticTest do
         "00010",
         "01010"
       ]
+
+    challenge = File.stream!("resources/03_report.txt")
+
+    {:ok, sample: sample, challenge: challenge}
+  end
+
+  test "1st part. Sampel input", %{sample: input} do
     assert BinaryDiagnostic.power_comsumption(input) == 198
   end
 
-  test "2nd part. AoC input" do
-    input =
-      [
-        "00100",
-        "11110",
-        "10110",
-        "10111",
-        "10101",
-        "01111",
-        "00111",
-        "11100",
-        "10000",
-        "11001",
-        "00010",
-        "01010"
-      ]
-    assert BinaryDiagnostic.life_support(input) == 230
-  end
-
-  test "1st part. AoC challenge" do
-    input =
-      "resources/03_report.txt"
-      |> File.stream!()
-
+  test "1st part. AoC challenge", %{challenge: input} do
     assert BinaryDiagnostic.power_comsumption(input) == 3633500
   end
 
-  test "2nd part. AoC challenge" do
-    input =
-      "resources/03_report.txt"
-      |> File.stream!()
+  test "2nd part. Sample input", %{sample: input} do
+    assert BinaryDiagnostic.life_support(input) == 230
+  end
 
+  test "2nd part. AoC challenge", %{challenge: input} do
     assert BinaryDiagnostic.life_support(input) == 4550283
   end
 end
